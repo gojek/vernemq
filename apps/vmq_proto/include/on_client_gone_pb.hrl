491e6c9eea2fd9799dbc1496c214f11d75258b76
Ken Kalang <ken.qalyubi@gojek.com>
2025-11-17 20:11:13 +0700

    fix: fix lint error

--- apps/vmq_proto/include/on_client_gone_pb.hrl
+++ apps/vmq_proto/include/on_client_gone_pb.hrl
@@ -5,26 +5,55 @@
 -ifndef(on_client_gone_pb).
 -define(on_client_gone_pb, true).
 
--define(on_client_gone_pb_gpb_version, "4.21.5").
-
+-define(on_client_gone_pb_gpb_version, "4.20.0").
 
 -ifndef('EVENTSSIDECAR.V1.ONCLIENTGONE_PB_H').
 -define('EVENTSSIDECAR.V1.ONCLIENTGONE_PB_H', true).
 -record('eventssidecar.v1.OnClientGone',
-        {timestamp = undefined  :: on_client_gone_pb:'google.protobuf.Timestamp'() | undefined, % = 1, optional
-         client_id = <<>>       :: unicode:chardata() | undefined, % = 2, optional
-         mountpoint = <<>>      :: unicode:chardata() | undefined, % = 3, optional
-         reason = 'REASON_UNSPECIFIED' :: 'REASON_UNSPECIFIED' | 'REASON_NOT_AUTHORIZED' | 'REASON_NORMAL_DISCONNECT' | 'REASON_SESSION_TAKEN_OVER' | 'REASON_ADMINISTRATIVE_ACTION' | 'REASON_DISCONNECT_KEEP_ALIVE' | 'REASON_DISCONNECT_MIGRATION' | 'REASON_BAD_AUTHENTICATION_METHOD' | 'REASON_REMOTE_SESSION_TAKEN_OVER' | 'REASON_MQTT_CLIENT_DISCONNECT' | 'REASON_RECEIVE_MAX_EXCEEDED' | 'REASON_PROTOCOL_ERROR' | 'REASON_PUBLISH_AUTH_ERROR' | 'REASON_INVALID_PUBREC_ERROR' | 'REASON_INVALID_PUBCOMP_ERROR' | 'REASON_UNEXPECTED_FRAME_TYPE' | 'REASON_EXIT_SIGNAL_RECEIVED' | 'REASON_TCP_CLOSED' | integer() | undefined, % = 4, optional, enum eventssidecar.v1.Reason
-         username = <<>>        :: unicode:chardata() | undefined % = 5, optional
-        }).
+    % = 1, optional
+    {
+        timestamp = undefined :: on_client_gone_pb:'google.protobuf.Timestamp'() | undefined,
+        % = 2, optional
+        client_id = <<>> :: unicode:chardata() | undefined,
+        % = 3, optional
+        mountpoint = <<>> :: unicode:chardata() | undefined,
+        % = 4, optional, enum eventssidecar.v1.Reason
+        reason = 'REASON_UNSPECIFIED' ::
+            'REASON_UNSPECIFIED'
+            | 'REASON_NOT_AUTHORIZED'
+            | 'REASON_NORMAL_DISCONNECT'
+            | 'REASON_SESSION_TAKEN_OVER'
+            | 'REASON_ADMINISTRATIVE_ACTION'
+            | 'REASON_DISCONNECT_KEEP_ALIVE'
+            | 'REASON_DISCONNECT_MIGRATION'
+            | 'REASON_BAD_AUTHENTICATION_METHOD'
+            | 'REASON_REMOTE_SESSION_TAKEN_OVER'
+            | 'REASON_MQTT_CLIENT_DISCONNECT'
+            | 'REASON_RECEIVE_MAX_EXCEEDED'
+            | 'REASON_PROTOCOL_ERROR'
+            | 'REASON_PUBLISH_AUTH_ERROR'
+            | 'REASON_INVALID_PUBREC_ERROR'
+            | 'REASON_INVALID_PUBCOMP_ERROR'
+            | 'REASON_UNEXPECTED_FRAME_TYPE'
+            | 'REASON_EXIT_SIGNAL_RECEIVED'
+            | 'REASON_TCP_CLOSED'
+            | integer()
+            | undefined,
+        username = <<>>        :: unicode:chardata() | undefined % = 5, optional
+    }
+).
 -endif.
 
 -ifndef('GOOGLE.PROTOBUF.TIMESTAMP_PB_H').
 -define('GOOGLE.PROTOBUF.TIMESTAMP_PB_H', true).
 -record('google.protobuf.Timestamp',
-        {seconds = 0            :: integer() | undefined, % = 1, optional, 64 bits
-         nanos = 0              :: integer() | undefined % = 2, optional, 32 bits
-        }).
+    % = 1, optional, 64 bits
+    {
+        seconds = 0 :: integer() | undefined,
+        % = 2, optional, 32 bits
+        nanos = 0 :: integer() | undefined
+    }
+).
 -endif.
 
 -endif.
