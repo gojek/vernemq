cc9b485d141e973e1aab67f92ed08ea82df79103
Ken Kalang <ken.qalyubi@gojek.com>
2026-02-03 11:53:36 +0700

    feat: enable auth_on_register on sidecar plugin

--- apps/vmq_events_sidecar/src/vmq_events_sidecar_plugin.erl
+++ apps/vmq_events_sidecar/src/vmq_events_sidecar_plugin.erl
@@ -7,6 +7,7 @@
 -include_lib("vmq_commons/src/vmq_types_common.hrl").
 
 -behaviour(gen_server).
+-behaviour(auth_on_register_hook).
 -behaviour(on_register_hook).
 -behaviour(on_publish_hook).
 -behaviour(on_subscribe_hook).
@@ -21,6 +22,7 @@
 -behaviour(on_message_drop_hook).
 
 -export([
+    auth_on_register/5,
     on_register/4,
     on_publish/7,
     on_subscribe/3,
@@ -249,6 +251,13 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Hook functions
 %%%===================================================================
+-spec auth_on_register(peer(), subscriber_id(), username(), password(), flag()) -> 'next'.
+auth_on_register(Peer, SubscriberId, UserName, _, CleanSession) ->
+    {PPeer, Port} = peer(Peer),
+    {MP, ClientId} = subscriber_id(SubscriberId),
+    send_event(auth_on_register, {MP, ClientId, PPeer, Port, normalise(UserName), CleanSession}),
+    next.
+
 %% called as an all_till_ok hook
 -spec on_register(peer(), subscriber_id(), username(), properties()) -> 'next'.
 on_register(Peer, SubscriberId, UserName, Props) ->
