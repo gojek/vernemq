2523a491e4dd12f016a29582661fecf4031a2f57
Ken Kalang <ken.qalyubi@gojek.com>
2026-05-13 17:08:44 +0700

    feat: add config for cluster send and publish timeout

--- apps/vmq_server/src/vmq_config_cli.erl
+++ apps/vmq_server/src/vmq_config_cli.erl
@@ -64,7 +64,9 @@ register_config_() ->
             "queue_sup_sup_max_t",
             "queue_sup_sup_max_r",
             "cache_shared_subscriptions_locally",
-            "direct_message_passing"
+            "direct_message_passing",
+            "cluster_publish_timeout",
+            "cluster_send_timeout"
         ],
     _ = [
         clique:register_config([Key], fun register_config_callback/2)
