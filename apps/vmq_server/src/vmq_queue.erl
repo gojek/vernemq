b5bf83b8622956869706f2372714dea5de4ecb6d
kenkalang <kenkalang@yahoo.com>
2026-07-17 15:16:48 +0700

    fix: QoS0 offline queue_unhandled metric

--- apps/vmq_server/src/vmq_queue.erl
+++ apps/vmq_server/src/vmq_queue.erl
@@ -1129,6 +1129,7 @@ insert(
     Sessions == #{}
 ->
     %% no session online, skip message for QoS0 Subscription
+    _ = vmq_metrics:incr_queue_unhandled(1),
     on_message_drop_hook(SId, D, ?USER_OFFLINE, SessionId),
     State;
 insert(#deliver{msg = #vmq_msg{non_persistence = true}}, #state{sessions = Sessions} = State) when
@@ -1144,6 +1145,7 @@ insert(
     Sessions == #{}
 ->
     %% no session online, skip QoS0 message for QoS1 or QoS2 Subscription
+    _ = vmq_metrics:incr_queue_unhandled(1),
     on_message_drop_hook(SId, D, ?USER_OFFLINE, SessionId),
     State;
 insert(
