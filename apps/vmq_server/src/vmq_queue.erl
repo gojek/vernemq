02304662de6bcd60e48b90e26ee413c63879f779
kenkalang <kenkalang@yahoo.com>
2026-07-20 01:17:31 +0700

    fix: QoS0 persist on upgrade_qos

--- apps/vmq_server/src/vmq_queue.erl
+++ apps/vmq_server/src/vmq_queue.erl
@@ -1146,11 +1146,12 @@ insert(#deliver{msg = #vmq_msg{non_persistence = true}}, #state{sessions = Sessi
     State;
 insert(
     #deliver{msg = #vmq_msg{qos = 0}} = D,
-    #state{id = SId, sessions = Sessions, session_id = SessionId} = State
+    #state{id = SId, sessions = Sessions, session_id = SessionId, opts = #{upgrade_qos := false}} =
+        State
 ) when
     Sessions == #{}
 ->
-    %% no session online, skip QoS0 message for QoS1 or QoS2 Subscription
+    %% no session online, skip QoS0 message for QoS1 or QoS2 Subscription (without QoS upgrade)
     _ = vmq_metrics:incr_queue_unhandled(1),
     on_message_drop_hook(SId, D, ?USER_OFFLINE, SessionId),
     State;
